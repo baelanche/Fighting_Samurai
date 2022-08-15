ed985d18da7582875eda0e19312fe617c9100b99
baegyu <baelanche@naver.com>
2022-08-15 16:31:27 +0900

    move character

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ canvas.height = 576
 
 c.fillRect(0, 0, canvas.width, canvas.height)
 
-const gravity = 0.2
+const gravity = 0.7
 
 class Sprite {
   constructor({ position, velocity }) {
@@ -22,7 +22,8 @@ class Sprite {
 
   update() {
     this.draw()
-    
+
+    this.position.x += this.velocity.x
     this.position.y += this.velocity.y
 
     if (this.position.y + this.height + this.velocity.y >= canvas.height) {
@@ -55,12 +56,58 @@ const enemy = new Sprite({
   }
 })
 
+const keys = {
+  ArrowLeft: {
+    pressed: false
+  },
+  ArrowRight: {
+    pressed: false
+  },
+}
+
+let lastKey
+
 function animate() {
   window.requestAnimationFrame(animate)
   c.fillStyle = 'black'
   c.fillRect(0, 0, canvas.width, canvas.height)
   player.update()
   enemy.update()
+
+  player.velocity.x = 0
+
+  if (keys.ArrowLeft.pressed && lastKey === 'ArrowLeft') {
+    player.velocity.x = -5
+  } else if (keys.ArrowRight.pressed && lastKey === 'ArrowRight') {
+    player.velocity.x = 5
+  }
 }
 
 animate()
+
+window.addEventListener('keydown', (event) => {
+  switch (event.key) {
+    case 'ArrowLeft':
+      keys.ArrowLeft.pressed = true
+      lastKey = 'ArrowLeft'
+      break
+    case 'ArrowRight':
+      keys.ArrowRight.pressed = true
+      lastKey = 'ArrowRight'
+      break
+    case 'ArrowUp':
+      player.velocity.y = -15
+      break
+  }
+})
+
+window.addEventListener('keyup', (event) => {
+  switch (event.key) {
+    case 'ArrowLeft':
+      keys.ArrowLeft.pressed = false
+      break
+    case 'ArrowRight':
+      keys.ArrowRight.pressed = false
+      break
+  }
+})
